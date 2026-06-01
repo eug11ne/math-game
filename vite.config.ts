0cfe46893483934835ceef6a4d6ab45381b59627
Evg3ny <evgeny.c3po@gmail.com>
2026-06-01 08:18:13 +0300

    seo add

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: resolve(__dirname, 'static'),
   server: {
-    port: 3000, 
-    open: true, 
+    port: 3000,
+    open: true,
   },
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
 });
